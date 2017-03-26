c88c492dee13e8dbdb6c2c751ef4e7d1e65bec7f
ZbigniewS <zbigniewsitarz@gmail.com>
2017-03-26 22:40:48 +0200

    First version

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// plik scripts.js
+
+var triangleArea = 0;
+var a = prompt('Podaj wartość podstawy trójkąta');
+var h = prompt('Podaj wartość wysokości trójkąta');
+// var a = 5;
+// var h = 6;
+triangleArea = a*h/2;
+alert('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: '+ h + ' wynosi: ' + triangleArea);
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: '+ h + ' wynosi: ' + triangleArea);
